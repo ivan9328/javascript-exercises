45554fadc689a7ccb053bedcdb5cff0657960827
Ivan Quan <ivan9328@gmail.com>
2022-08-17 00:07:13 -0400

    Complete exercises 1-7

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(theArray, ...remove) {
 
+    for(let i = 0; i < theArray.length; i++) {
+        if(contains(theArray[i], remove)) {
+            theArray.splice(i, 1)
+            i -= 1;
+        }
+    }
+    return theArray
 };
+
+function contains(element, array) {
+    for(let i = 0; i < array.length; i++) {
+        if(element === array[i]) return true
+    }
+    return false
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(n, m) {
+    sum = 0;
 
+    if(!Number.isInteger(n) || !Number.isInteger(m)) {return "ERROR"}
+
+    if(n < 0 || m < 0) {return "ERROR"}
+
+    if(n < m) {
+        for(let i = n; i <= m; i++) {
+            sum += i;
+        }
+    }
+    else {
+        for(let i = m; i <= n; i++) {
+            sum += i;
+        }
+    }
+    return sum
 };
 
 // Do not edit below this line
